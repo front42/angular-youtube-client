a9f799419879f3c8c4f6f5812ad835158cc74eae
front42 <altaifront@gmail.com>
2025-07-22 14:42:25 +0300

    init: add base project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  // imports: [RouterOutlet],
+  imports: [],
+})
+export class AppComponent {
+  title = 'YouTube Client';
+}
